ed50b3b84b8e32f6276723ae752b433e2200569c
PatZ1Z <patryk.witkowski@cyfroweszkoly.pl>
2026-02-18 09:20:03 +0100

    Seed.js creates Mongoflix database.

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,17 @@
-test
+db = db.getSiblingDB("Mongoflix")
+
+db.movies.drop()
+
+print("--- Inicjalizacja bazy danych Mongoflix ---")
+
+const fs = require("fs")
+const path = require("path")
+
+const filePath = path.join(__dirname, "../data/test.json")
+
+const fileContent = fs.readFileSync(filePath, "utf8")
+const data = JSON.parse(fileContent)
+
+db.movies.insertMany(data)
+
+print("--- działa ---")
